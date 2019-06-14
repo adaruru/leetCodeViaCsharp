4c34e16f8463b889b37f970ab146752dd4686441
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:31:27 +0800

    Create Judgezeroa010_PrimeFactorization.cs

---
+++ CsharpStartTest/Judgezeroa010_PrimeFactorization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Judgezeroa010_PrimeFactorization
+    {
+        public static void main()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int inputNumber = number;
+            SortedList<int, int> primaryList = new SortedList<int, int>();
+
+            for (int i = 2; i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    if (primaryList.ContainsKey(i))
+                    {
+                        primaryList[i] += 1;
+                    }
+                    else
+                    {
+                        primaryList.Add(i, 1);
+                    }
+                    number = number / i;
+                }
+            }
+            Console.Write(inputNumber + "=");
+
+            string output = ""; // string.Empty
+            foreach (int primary in primaryList.Keys)
+            {
+                if (output != "")
+                {
+                    output += "*";
+                }
+                output += primary + "^" + primaryList[primary];
+            }
+            Console.Write(output);
+        }
+
+    }
+}
