1fd0c45281ee91efc4be3bafe447c3d78f786582
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:32:52 +0800

    Create Leet007_ReverseInteger_1.cs

---
+++ CsharpStartTest/Leet007_ReverseInteger_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Leet007_ReverseInteger_1
+    {
+        public static void main()
+        {
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write(reverse(inputNum));
+        }
+        static int reverse(int intNum)
+        {
+            long retNum = 0;
+            long longNum = (long)intNum;
+            while (longNum != 0)
+            {
+                retNum = retNum * 10 + longNum % 10;
+                longNum /= 10;
+            }
+            if (retNum < -(Math.Pow(2, 31)) || retNum > (Math.Pow(2, 31)) - 1)
+            { return 0; }
+            int result = (int)retNum;
+            return result;
+        }
+    }
+}
