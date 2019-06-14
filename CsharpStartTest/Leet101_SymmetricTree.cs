73872b50576cd1bd5a2cf0b3c36ee4866a6a159d
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:33:37 +0800

    Create Leet101_SymmetricTree.cs

---
+++ CsharpStartTest/Leet101_SymmetricTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Leet101_SymmetricTree
+    {
+        public static void main()
+        {
+            //實作一個treenode
+            TreeNode root = new TreeNode(1);
+            root.left =new TreeNode(2);
+            root.right = new TreeNode(2);
+            root.left.left = new TreeNode(3);
+            root.right.right = new TreeNode(3);
+            root.right.left = new TreeNode(4);
+            root.left.right = new TreeNode(4);
+
+            bool result=IsSymmetric(root);
+            Console.Write(result);
+        }
+        static bool IsSymmetric (TreeNode root)
+        {
+            return true;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
