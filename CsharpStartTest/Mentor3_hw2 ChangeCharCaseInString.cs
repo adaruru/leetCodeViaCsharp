295f3dce1a0f6840e5eaffcd5d167495fa22bb0e
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:33:35 +0800

    Create Mentor3_hw2 ChangeCharCaseInString.cs

---
+++ CsharpStartTest/Mentor3_hw2 ChangeCharCaseInString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Mentor3_hw2_ChangeCharCaseInString
+    {
+        public static void main() {
+            string Str = Console.ReadLine();
+            string StrChange = ChangeCase(Str);
+            Console.WriteLine(StrChange);
+        }
+        static string ChangeCase(string s) {
+            string ChangedS = "";
+            for (int i = 0; i < s.Length; i++) {
+
+                if (Char.IsLower(s[i]))
+                {
+                    ChangedS += s[i].ToString().ToUpper();
+                }
+                else
+                {
+                    ChangedS += s[i].ToString().ToLower();
+                }
+            }
+            return ChangedS;
+        }
+
+    }
+}
