1028fdb4daab9fc03b215ff7d81f30b69bd6ee2a
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:31:31 +0800

    Create Program.cs

---
+++ CsharpStartTest/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CsharpStartTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BubbleSort.main();
+            //Day4_Class_vs_Instance.main();
+
+
+
+            //JudgeZeroa022_PalindromeString.main();
+            //Judgezeroa010_PrimeFactorization.main();
+
+            //Leet001_2Sum1.main();
+            //Leet001_2Sum2.main();
+            //Leet001_2Sum3.main();
+            //Leet001_2Sum4.main();
+            //Leet002_QustAddTwoNumber.main();
+            //Leet007_ReverseInteger_1.main();
+            //Leet007_ReverseInteger_2.main();
+            Leet008_StringToIntegerATOI_1.main();
+            //Leet008_StringToIntegerATOI_2.main();
+            //Leet009_PalindromeNumber.main();
+            //Leet012_IntegerToRoman.main();
+            //Leet013_RomanToInteger.main();
+            //Leet015_3Sum1.main();
+            //Leet015_3Sum2.main();
+
+            //Leet020_ValidParentheses.main();
+
+            //Leet058_LengthOfLastWord.main();
+            //Leet079_WordSearch1.main();
+            //Leet079_WordSearch2.main();
+            //Leet069_Sqrt.main();
+
+            //Leet101_SymmetricTree.main();
+
+            //Mentor3_hw2_ChangeCharCaseInString.main();
+
+
+
+
+            //printStar.main();
+
+
+
+        }
+
+    }
+   
+}
+
+
+
+
+
+
+
