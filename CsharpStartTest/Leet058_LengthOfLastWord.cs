be4d3de5758b8581aa854f29e649ed57ec167185
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:32:59 +0800

    Create Leet058_LengthOfLastWord.cs

---
+++ CsharpStartTest/Leet058_LengthOfLastWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Leet058_LengthOfLastWord
+    {
+        public static void main()
+        {
+            string str = Console.ReadLine();
+            Console.Write(LengthOfLastWord(str));
+        }
+        static int LengthOfLastWord(string s)
+        {
+            //移除多於空格，變成以空格輸入array, 最後一個, 的長度
+            //return s.Trim().Split().LastOrDefault().Length;
+            //return s.Trim().Split().Last().Length;
+            string[] strArr = s.Split();
+            string target = null;
+            for (int i = strArr.Length - 1; i >= 0; i--)
+            {
+                if (!string.IsNullOrEmpty(strArr[i]))
+                {
+                    target = strArr[i];
+                    break;
+                }
+            }
+
+            if (target != null) return target.Length;
+            else return 0;
+        }
+
+    }
+}
