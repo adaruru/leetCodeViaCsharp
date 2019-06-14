fee522fe0a3d2d8cc6b5f27384c26373679416f2
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-14 11:33:16 +0800

    Create Leet009_PalindromeNumber.cs

---
+++ CsharpStartTest/Leet009_PalindromeNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Leet009_PalindromeNumber
+    {
+        public static void main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsPalindrome(a));
+        }
+        static bool IsPalindrome(int x)
+        {
+            if (x <= 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+            int reverseNum = 0;
+            while (x > reverseNum)
+            {
+                reverseNum = reverseNum * 10 + x % 10;
+                x /= 10;
+            }
+            return x == reverseNum || x == reverseNum / 10;
+        }
+    }
+}
