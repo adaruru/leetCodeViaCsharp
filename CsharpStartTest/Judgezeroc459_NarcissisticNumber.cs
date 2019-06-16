7017b03a2ac85923b06a071eadf5ca63c8004c14
adaruru <amanda1991910@g.ncu.edu.tw>
2019-06-16 23:37:24 +0800

    Create Judgezeroc459_NarcissisticNumber.cs

---
+++ CsharpStartTest/Judgezeroc459_NarcissisticNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpStartTest
+{
+    class Judgezeroc459_NarcissisticNumber
+    {
+        public static void main() {
+            int d = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            Console.Write(NarcissisticCheck(d,N));
+        }
+        static string NarcissisticCheck(int d, int N) {
+
+            if (true)
+                return "yes";
+            else
+                return "no";
+        }
+    }
+}
