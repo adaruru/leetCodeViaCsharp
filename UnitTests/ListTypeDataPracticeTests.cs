9421312e94271dfb02cf1c041b2a9dca3f1b5708
Amanda <amanda.chou@frog-jump.com>
2021-08-25 23:45:30 +0800

    update unitTest

---
+++ UnitTests/ListTypeDataPracticeTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dataProcessCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace dataProcessCheckTests
+{
+    [TestClass()]
+    public class ListTypeDataPracticeTests
+    {
+        ListTypeDataPractice service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            service = new ListTypeDataPractice();
+        }
+
+        [TestMethod()]
+        public void ListOrderByTest()
+        {
+            //arrange
+            List<string> input = new List<string> { "1", "A", "B", "11", "10", "2", "03", "1" };
+            List<string> expected = new List<string> { "1", "A", "B", "11", "10", "2", "03", "1" };
+
+            //act
+            var output = service.ListOrderBy(input);
+
+            //assert
+            // Assert.AreEqual(expected, output);
+            CollectionAssert.AreEqual(expected, output, StructuralComparisons.StructuralComparer);
+
+        }
+    }
+}
