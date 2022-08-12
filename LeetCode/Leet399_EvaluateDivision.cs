32d307e0dbfd13de2cc264c4c482b4f63de23d8e
AmandaChou <AmandaChou@itsower.com.tw>
2022-08-12 23:00:15 +0800

    no message

--- LeetCode/Leet399_EvaluateDivision.cs
+++ LeetCode/Leet399_EvaluateDivision.cs
@@ -1,19 +1,25 @@
 ﻿namespace LeetCode;
 
 /// <summary>
-///You are given an array of variable pairs equations and an array of real numbers values,
-///where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. 
-///Each Ai or Bi is a string that represents a single variable.
+/// #medium Depth-First Search(DFS，深度優先搜尋)
+/// 
+/// You are given an array of variable pairs equations and an array of real numbers values,
+/// where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. 
+/// Each Ai or Bi is a string that represents a single variable.
 ///
-///You are also given some queries, where queries[j] = [Cj, Dj] 
-///represents the jth query where you must find the answer for Cj / Dj = ?.
+/// You are also given some queries, where queries[j] = [Cj, Dj] 
+/// represents the jth query where you must find the answer for Cj / Dj = ?.
 ///
-///Return the answers to all queries.
-///If a single answer cannot be determined, return -1.0.
+/// Return the answers to all queries.
+/// If a single answer cannot be determined, return -1.0.
+/// 返回 所有問題的答案 。如果存在某個無法確定的答案，則用 -1.0 替代這個答案。
 ///
-///Note: The input is always valid. 
-///You may assume that evaluating the queries will not result in division by zero 
-///and that there is no contradiction.
+/// Note: The input is always valid. 
+/// 注意：輸入總是有效的。
+/// You may assume that evaluating the queries will not result in division by zero
+/// 你可以假設除法運算中不會出現除數為 0 的情況
+/// and that there is no contradiction.
+/// 且不存在任何矛盾的結果。
 ///
 /// Input: equations = [["a","b"],["b","c"]]
 /// values = [2.0,3.0]
@@ -39,7 +45,7 @@ public class Leet399_EvaluateDivision
         Console.Write("Leet399_EvaluateDivision");
 
         IList<IList<string>> Input = new List<IList<string>> { new List<string> { "a", "b" }, new List<string> { "b", "c" } };
-        double[] values = [2.0, 3.0];
+        double[] values = { 2.0, 3.0 };
         IList<IList<string>> queries = new List<IList<string>> {
              new List<string> {"a", "c"},
              new List<string> {"b", "a"},
@@ -52,7 +58,29 @@ public class Leet399_EvaluateDivision
     }
     public static double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
     {
+        var routeMap = new Dictionary<List<string>, double>();
+        for (int i = 0; i < equations.Count; i++)
+        {
+            double ov = 0;
+
+            if (!routeMap.TryGetValue(equations[i].ToList(), out ov))//加入正續
+            {
+                routeMap.Add(equations[i].ToList(), values[i]);
+            }
+            if (!routeMap.TryGetValue(new List<string> { equations[i][1], equations[i][0] }, out ov))//加入倒續
+            {
+                routeMap.Add(equations[i].ToList(), 1 / values[i]);
+            }
+
+            for (int j = 0; j < queries.Count(); j++)
+            {
+
+            }
+
+
+        }
         return new double[] { };
     }
+
 }
 
